b59dbec5d45cd786d8bda03d51fabfcdb1066ab0
Florent Fougères <florent.fougeres@oslandia.com>
2023-09-27 15:01:16 +0200

    Test ihm method

---
+++ tests/qgis/test_dlg_add_duckdb_layer.py
@@ -0,0 +1,50 @@
+import unittest
+from pathlib import Path
+
+from qgis.core import QgsProject
+from qgis.testing import start_app
+
+from qduckdb.gui.dlg_add_duckdb_layer import LoadDuckDBLayerDialog
+
+start_app()
+
+
+class TestDlgAddDuckdbLayer(unittest.TestCase):
+    def __init__(self, methodName: str) -> None:
+        super().__init__(methodName)
+        self.dialog = LoadDuckDBLayerDialog()
+        self.db_path_test = (
+            Path(__file__).parent.joinpath("data/base_test.db").as_posix()
+        )
+        self.wrong_db_path = "wrong/path/zidane.db"
+
+    def test_get_path(self) -> None:
+        """Check that the database path is correctly returned"""
+        # Good path
+        self.dialog._db_path_input.setFilePath(self.db_path_test)
+        self.assertEqual(self.dialog.db_path, self.db_path_test)
+        # Wrong path
+        self.dialog._db_path_input.setFilePath(self.wrong_db_path)
+        self.assertEqual(self.dialog.db_path, self.wrong_db_path)
+
+    def test_list_table_in_db(self) -> None:
+        """We test that the list of tables in the database is correctly returned"""
+        self.dialog._db_path_input.setFilePath(self.db_path_test)
+        self.assertEqual(
+            self.dialog.list_table_in_db,
+            ["table_no_geom", "highway", "building", "cities"],
+        )
+
+    def test_push_add_layer_button(self) -> None:
+        """Test that a layer has been added to the canvas"""
+        self.dialog._db_path_input.setFilePath(self.db_path_test)
+        self.dialog._table_combobox.setCurrentText("highway")
+        self.dialog._db_path_input.setFilePath(self.db_path_test)
+        self.dialog._push_add_layer_button()
+        project = QgsProject.instance()
+        self.assertTrue(project.mapLayersByName("highway"))
+
+    def test_lock_button_with_wrong_path(self) -> None:
+        """We test that the button remains locked when the wrong base is entered."""
+        self.dialog._db_path_input.setFilePath(self.wrong_db_path)
+        self.assertFalse(self.dialog._add_layer_btn.isEnabled())
