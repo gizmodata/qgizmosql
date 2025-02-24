000e3ad03d12d10b38b716339dd65918de7b3c0e
Florent Fougères <florent.fougeres@oslandia.com>
2025-02-24 11:11:03 +0100

    i18n: add missing french translation

--- qduckdb/resources/i18n/qduckdb_fr.ts
+++ qduckdb/resources/i18n/qduckdb_fr.ts
@@ -31,42 +31,42 @@
     <message>
         <location filename="../../gui/dlg_add_duckdb_layer.ui" line="38"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Custom SQL query&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Requête SQL personnalisée&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_add_duckdb_layer.ui" line="52"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Extension&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Extension&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_add_duckdb_layer.ui" line="112"/>
         <source>Entire table</source>
-        <translation type="unfinished"></translation>
+        <translation>Table entière</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_add_duckdb_layer.ui" line="122"/>
         <source>Custom SQL query</source>
-        <translation type="unfinished"></translation>
+        <translation>Requête SQL personnalisée</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_open_parquet.ui" line="26"/>
         <source>Open Parquet with DuckDB</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir Parquet avec DuckDB</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_open_parquet.ui" line="32"/>
         <source>Open (Geo)Parquet files with DuckDB</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir des fichiers (Geo)Parquet avec DuckDB</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_open_parquet.ui" line="38"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;You can open one or more parquet files and choose whether or not to group the results. To do this, the plugin doesn&apos;t use QGIS&apos;s native parquet provider; a DuckDB memory base will be created to read the parquet file(s), then the plugin&apos;s QDuckDB provider will be used to create the layer in QGIS.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;Vous pouvez ouvrir un ou plusieurs fichiers parquet et choisir de grouper ou non les résultats. Pour ce faire, le plugin n&apos;utilise pas le fournisseur de parquet natif de QGIS ; une base de mémoire DuckDB sera créée pour lire le(s) fichier(s) parquet, puis le fournisseur QDuckDB du plugin sera utilisé pour créer la couche dans QGIS.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../../gui/dlg_open_parquet.ui" line="61"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir</translation>
     </message>
 </context>
 <context>
@@ -74,22 +74,22 @@
     <message>
         <location filename="../../provider/duckdb_provider.py" line="164"/>
         <source>{} unknown extension, open an issue if it exists to add its support.</source>
-        <translation type="unfinished"></translation>
+        <translation>{} extension inconnue, ouvrez une question si elle existe pour ajouter son support.</translation>
     </message>
     <message>
         <location filename="../../provider/duckdb_provider.py" line="193"/>
         <source>The sql query is invalid: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>La requête SQL n&apos;est pas valide : {}</translation>
     </message>
     <message>
         <location filename="../../provider/duckdb_provider.py" line="259"/>
         <source>Geometry type {} not supported</source>
-        <translation type="unfinished"></translation>
+        <translation>Le type de géométrie {} n&apos;est pas pris en charge</translation>
     </message>
     <message>
         <location filename="../../provider/duckdb_provider.py" line="480"/>
         <source>SQL error in filter : {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur SQL dans le filtre : {}</translation>
     </message>
 </context>
 <context>
@@ -97,7 +97,7 @@
     <message>
         <location filename="../../gui/dlg_add_duckdb_layer.py" line="146"/>
         <source>The database {} does not exist.</source>
-        <translation type="unfinished"></translation>
+        <translation>La base de données {} n&apos;existe pas.</translation>
     </message>
 </context>
 <context>
@@ -105,7 +105,7 @@
     <message>
         <location filename="../../gui/dlg_open_parquet.py" line="73"/>
         <source>The parquet file {} does not exist.</source>
-        <translation type="unfinished"></translation>
+        <translation>Le fichier parquet {} n&apos;existe pas.</translation>
     </message>
 </context>
 <context>
@@ -113,37 +113,37 @@
     <message>
         <location filename="../../plugin_main.py" line="127"/>
         <source>Help</source>
-        <translation type="unfinished">Aide</translation>
+        <translation>Aide</translation>
     </message>
     <message>
         <location filename="../../plugin_main.py" line="136"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Paramètres</translation>
     </message>
     <message>
         <location filename="../../plugin_main.py" line="147"/>
         <source>DuckDB</source>
-        <translation type="unfinished">DuckDB</translation>
+        <translation>DuckDB</translation>
     </message>
     <message>
         <location filename="../../plugin_main.py" line="155"/>
         <source>Open Parquet with DuckDB</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir Parquet avec DuckDB</translation>
     </message>
     <message>
         <location filename="../../plugin_main.py" line="256"/>
         <source>Error importing dependencies. Plugin disabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur dans l&apos;importation des dépendances. Plugin désactivé.</translation>
     </message>
     <message>
         <location filename="../../plugin_main.py" line="272"/>
         <source>Plugin disabled. Please install all dependencies and then restart QGIS. Refer to the documentation for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Plugin désactivé. Veuillez installer toutes les dépendances et redémarrer QGIS. Reportez-vous à la documentation pour plus d&apos;informations.</translation>
     </message>
     <message>
         <location filename="../../plugin_main.py" line="279"/>
         <source>Dependencies satisfied</source>
-        <translation type="unfinished"></translation>
+        <translation>Dépendances satisfaites</translation>
     </message>
 </context>
 <context>
@@ -151,12 +151,12 @@
     <message>
         <location filename="../../plugin_main.py" line="295"/>
         <source>Error importing dependencies. Plugin disabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur dans l&apos;importation des dépendances. Plugin désactivé.</translation>
     </message>
     <message>
         <location filename="../../plugin_main.py" line="303"/>
         <source>Dependencies satisfied</source>
-        <translation type="unfinished"></translation>
+        <translation>Dépendances satisfaites</translation>
     </message>
 </context>
 <context>
