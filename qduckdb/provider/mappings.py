2ec0c61b105d463d5fc484d7973e7036950b6086
Florent Fougères <florent.fougeres@oslandia.com>
2025-03-12 15:59:18 +0100

    mapping: QVariant types are now deprecated in the QgsField constrcutor, so you should use QMetaType.Type

--- qduckdb/provider/mappings.py
+++ qduckdb/provider/mappings.py
@@ -1,5 +1,5 @@
 from qgis.core import QgsWkbTypes
-from qgis.PyQt.QtCore import QVariant
+from qgis.PyQt.QtCore import QMetaType
 
 mapping_duckdb_qgis_geometry = {
     "LINESTRING": QgsWkbTypes.Type.LineString,
@@ -12,21 +12,21 @@ mapping_duckdb_qgis_geometry = {
 }
 
 mapping_duckdb_qgis_type = {
-    "BIGINT": QVariant.Int,
-    "BOOLEAN": QVariant.Bool,
-    "DATE": QVariant.Date,
-    "TIME": QVariant.Time,
-    "DOUBLE": QVariant.Double,
-    "FLOAT": QVariant.Double,
-    "INTEGER": QVariant.Int,
-    "TIMESTAMP": QVariant.DateTime,
-    "VARCHAR": QVariant.String,
+    "BIGINT": QMetaType.Type.Int,
+    "BOOLEAN": QMetaType.Type.Bool,
+    "DATE": QMetaType.Type.QDate,
+    "TIME": QMetaType.Type.QTime,
+    "DOUBLE": QMetaType.Type.Double,
+    "FLOAT": QMetaType.Type.Double,
+    "INTEGER": QMetaType.Type.Int,
+    "TIMESTAMP": QMetaType.Type.QDateTime,
+    "VARCHAR": QMetaType.Type.QString,
     # Type used for custom sql when table is not created
-    # Not difference betwenn float and integer so all the numeric field are NUMBER
-    "NUMBER": QVariant.Double,
-    "STRING": QVariant.String,
-    "Date": QVariant.Date,
-    "bool": QVariant.Bool,
-    "JSON": QVariant.String,
-    "DATETIME": QVariant.DateTime,
+    # No difference between float and integer so all numeric fields are NUMBER
+    "NUMBER": QMetaType.Type.Double,
+    "STRING": QMetaType.Type.QString,
+    "Date": QMetaType.Type.QDate,
+    "bool": QMetaType.Type.Bool,
+    "JSON": QMetaType.Type.QString,
+    "DATETIME": QMetaType.Type.QDateTime,
 }
