gizmodata/qgizmosql

commit 078b0e5def6fba204d8fb8d8d63d010c5ef30b69

feat: When the plugin starts, display an alert message if the user is using a version of DuckDB that is not the one with which the current version was developed.

qduckdb/plugin_main.py
@@ -39,6 +39,10 @@ try:
     from qduckdb.gui.dlg_add_duckdb_layer import LoadDuckDBLayerDialog
     from qduckdb.gui.dlg_open_parquet import OpenParquetDialog
     from qduckdb.provider.duckdb_provider_metadata import DuckdbProviderMetadata
+    from qduckdb.provider.duckdb_wrapper import (
+        DUCKDB_CURRENT_VERSION,
+        DUCKDB_SUPPORTED_VERSION,
+    )
 
     EXTERNAL_DEPENDENCIES_AVAILABLE = True
 except ImportError:
@@ -191,8 +195,17 @@ class QduckdbPlugin(QduckdbBasePlugin):
 
         # register custom provider
         self.register_duckdb_provider()
-
         QgsProject.instance().layersWillBeRemoved.connect(self._on_layers_removal)
+
+        # A warning is displayed if the user is using the plugin with a version of DuckDB
+        # that is different from the one with which the plugin was developed.
+
+        if DUCKDB_CURRENT_VERSION < DUCKDB_SUPPORTED_VERSION:
+            msg = self.tr(
+                "Please note that you are using the plugin with version {} of DuckDB, or the plugin was developed to work optimally with version {}. You may experience bugs or unexpected behavior."
+            ).format(DUCKDB_CURRENT_VERSION, DUCKDB_SUPPORTED_VERSION)
+
+            self.log(message=msg, log_level=1, push=True, duration=60)
 
     def unload(self):
         """Cleans up when plugin is disabled/uninstalled."""

qduckdb/provider/duckdb_wrapper.py
@@ -44,6 +44,7 @@ except Exception:
     )
 
 DUCKDB_CURRENT_VERSION = version.parse(duckdb.__version__)
+DUCKDB_SUPPORTED_VERSION = version.parse("1.4")
 
 
 # -- CLASSES --
@@ -620,9 +621,7 @@ class DuckDbTools:
             )
         self.retrieve_duckdb_extensions()
 
-    def parse_uri(
-        self, uri: str
-    ) -> tuple[
+    def parse_uri(self, uri: str) -> tuple[
         Optional[str],
         Optional[str],
         Optional[str],

commit 96b7c01e06275171528cb52c8a1b50a8215d5a5b

fix(plugin_main): Use an enum to set log messages level
Otherwise, this does not work on Qt6 builds.

Fixes: 078b0e5def6fba204d8fb8d8d63d010c5ef30b69

qduckdb/plugin_main.py
@@ -205,7 +205,9 @@ class QduckdbPlugin(QduckdbBasePlugin):
                 "Please note that you are using the plugin with version {} of DuckDB, or the plugin was developed to work optimally with version {}. You may experience bugs or unexpected behavior."
             ).format(DUCKDB_CURRENT_VERSION, DUCKDB_SUPPORTED_VERSION)
 
-            self.log(message=msg, log_level=1, push=True, duration=60)
+            self.log(
+                message=msg, log_level=Qgis.MessageLevel.Warning, push=True, duration=60
+            )
 
     def unload(self):
         """Cleans up when plugin is disabled/uninstalled."""